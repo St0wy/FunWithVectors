d84e50bd7863a2ffed404011f156494d7a5526d6
Stowy <stowy@protonmail.ch>
2023-01-30 12:36:29 +0100

    added rotating planet

--- src/main.cpp
+++ src/main.cpp
@@ -17,21 +17,33 @@ int main()
 
 	SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
 
+	constexpr float sunScale = 0.3f;
+	auto sunPos = stw::Vec2((WINDOW_WIDTH / 2.0f - sun.GetWidth() * sunScale / 2.0f),
+							(WINDOW_HEIGHT / 2.0f - sun.GetHeight() * sunScale / 2.0f));
+	constexpr float planetScale = 0.2f;
+	constexpr float planetSpeed = 5.0f;
+	constexpr float planetDistance = 300.0f;
 	stw::Vec2 planetPos{600, 170};
+	float planetAngle = 0.0f;
 
+	float deltaTime;
 	while (!window.ShouldClose())    // Detect window close button or ESC key
 	{
+		deltaTime = GetFrameTime();
 		BeginDrawing();
 
 		ClearBackground(BLACK);
 
+		planetAngle += planetSpeed * deltaTime;
+
 		DrawText("Moi j'aime les planètes.", 0, 0, 20, LIGHTGRAY);
 
-		constexpr float scale = 0.2f;
-		raylib::Vector2 pos = raylib::Vector2((WINDOW_WIDTH / 2.0f - sun.GetWidth() * scale / 2.0f),
-											  (WINDOW_HEIGHT / 2.0f - sun.GetHeight() * scale / 2.0f));
-		sun.Draw(pos, 0.0f, scale);
-		planet.Draw({planetPos.x, planetPos.y}, 0.0f, scale);
+		sun.Draw({sunPos.x, sunPos.y}, 0.0f, sunScale);
+
+		planetPos.x = std::cos(planetAngle) * planetDistance;
+		planetPos.y = std::sin(planetAngle) * planetDistance;
+		planetPos += sunPos;
+		planet.Draw({planetPos.x, planetPos.y}, 0.0f, planetScale);
 
 		EndDrawing();
 	}
